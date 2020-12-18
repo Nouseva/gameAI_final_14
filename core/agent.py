db46bccb09f0d11cb51645e58cce01159c5f7d34
DarrionNguyen <nguyendarrion@gmail.com>
2020-12-17 23:38:07 -0800

    print statements and stuff i think

--- core/agent.py
+++ core/agent.py
@@ -45,8 +45,11 @@ def default_heuristic(self, equiped_list, pos, layout):
     #if no enemy and no treasure
     elif 'Wheels' in equiped_list and (layout.isRoad(pos) or layout.isBoost(pos)):
         heur = 150
+    elif 'Flower' in equiped_list and layout.isEnemy(pos):
+        heur = -100
     else:
         heur = 100
+    print(heur)
     return heur
 
 class Agent(pygame.sprite.Sprite):
@@ -235,8 +238,8 @@ class Agent(pygame.sprite.Sprite):
             if (west.tup in visitedDict):
                 wValue -= 100 * visitedDict[west.tup]
         valuesArr[3] = wValue
-        # if (depth == 0):
-            # print(valuesArr)
+        if (depth == 0):
+             print(valuesArr)
         maxValue = max(valuesArr)
         keysArr = []
         for i in range(len(valuesArr)):
